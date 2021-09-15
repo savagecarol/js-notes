1ad7b7202d075f294ee4d7a730d96cb355d49790
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-09-15 14:43:11 +0530

    this is my last pushh maybe

--- notes.js
+++ notes.js
@@ -1,18 +1,18 @@
 // for printing cards
-console.log("DSFdsffd");
-let answer = new Array([","]);
-answer = localStorage.getItem("1");
-if(answer!==null) 
-{
-    answer.split(",");
+if(localStorage.getItem("1") !== null)
+    answer = Array.from(localStorage.getItem("1").split(","));
+else
+    answer = new Array([","]);
+console.log(answer);
+    
     let n = answer.length;
     console.log(n);
-    for(var i = 1 ; i < n ; i++)
+    for(var i = 2 ; i < n ; i++)
     {
     let p = document.getElementById("abcd");
     p.innerHTML+= `<div class = "card" >
                         <div class = "container">
-                        <h3> <b>  Note  ${i} </b> </h3>
+                        <h3> <b>  Note  ${i-1} </b> </h3>
                             <h6>
                                 ${answer[i]} 
                             </h6>
@@ -20,14 +20,10 @@ if(answer!==null)
                     </div> `;
     } 
 
-}
 // for text field
-
-const form  = document.getElementById('one');
+let form  = document.getElementById('one');
 let text = document.getElementById('data');
-form.addEventListener('click', (event) => {
-
-    event.preventDefault();
+form.addEventListener('click', () => {
     answer.push(text.value);
     localStorage.setItem("1" , answer);
     location.reload();
